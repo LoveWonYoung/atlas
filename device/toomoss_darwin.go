2424d0998fa87664e3361456987434738ee6b5f4
liaolianmin <168514731+LoveWonYoung@users.noreply.github.com>
2026-07-23 15:57:50 +0800

    master

--- device/toomoss_darwin.go
+++
@@ -1,9 +0,0 @@
-//go:build darwin && cgo
-
-package device
-
-import "github.com/LoveWonYoung/atlas/driver"
-
-func newDarwinToomossCAN(canType driver.CanType, channel byte) driver.CANDriver {
-	return driver.NewToomoss(canType, channel)
-}
